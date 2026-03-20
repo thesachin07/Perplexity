8bbf6dcc2f8e6e1caaf0db5368f1b45fbd57554f
Sachin Kumar <sachin070502@gmail.com>
2026-03-20 22:50:35 +0530

    feat: add auth pages and redux store integration

---
+++ Frontend/src/app/app.routes.jsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter } from "react-router";
+import Login from "../features/auth/pages/Login";
+import Register from "../features/auth/pages/Register";
+// import Dashboard from "../features/chat/pages/Dashboard";
+// import Protected from "../features/auth/components/Protected";
+import { Navigate } from "react-router";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Navigate to="/login" replace />
+    },
+    {
+        path: "/login",
+        element: <Login />
+    },
+    {
+        path: "/register",
+        element: <Register />
+    },
+    {
+        path: "/",
+        element: <h1>Home Page</h1>
+    }
+    // {
+    //     path: "/",
+    //     element: <Protected>
+    //         <Dashboard />
+    //     </Protected>
+    // },
+    // {
+    //     path: "/dashboard",
+    //     element: <Navigate to="/" replace />
+    // }
+])
