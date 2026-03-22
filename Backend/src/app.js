dec5c6c3c889101d4b5c90029df545d2e00a31b1
Sachin Kumar <sachin070502@gmail.com>
2026-03-22 23:51:24 +0530

    feat: add chat router and update app.js to include it, create .gitignore file

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
+import chatRouter from "./routes/chat.routes.js";
 
 const app = express();
 
