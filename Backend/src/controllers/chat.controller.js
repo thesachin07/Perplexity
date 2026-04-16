56ab0035b0f280bc9bd7ab58c6b4aadce94971d6
Sachin Kumar <sachin070502@gmail.com>
2026-04-16 13:46:49 +0530

    updates chats routes, chat controller and ai services

--- Backend/src/controllers/chat.controller.js
+++ Backend/src/controllers/chat.controller.js
@@ -1,11 +1,98 @@
-import { generateResponse } from "../services/ai.service.js";
-
+import { generateResponse, generateChatTitle } from "../services/ai.service.js";
+import chatModel from "../models/chat.model.js";
+import messageModel from "../models/message.model.js";
 
 export async function sendMessage(req, res) {
+  const { message, chat: chatId } = req.body;
 
-        const { message } = req.body;
-       
-        const result = await generateResponse(message);
-        res.json({ aiMessage : result });
+  let chatTitle = null,
+    chat = null;
 
+  if (!chatId) {
+    chatTitle = await generateChatTitle(message);
+    chat = await chatModel.create({
+      user: req.user.id,
+      title: chatTitle,
+    });
+  }
+
+  const userMessage = await messageModel.create({
+    chat: chat.id,
+    content: message,
+    role: "user",
+  });
+
+  const messages = await messageModel.find({ chat: chatId || chat.id });
+
+  const result = await generateResponse(message);
+
+  const aiMessage = await messageModel.create({
+    chat: chat._id,
+    content: result,
+    role: "ai",
+  });
+
+  res.status(201).json({
+    title: chatTitle,
+    chat,
+    aiMessage,
+  });
+}
+export async function getChats(req, res) {
+    const user = req.user
+
+    const chats = await chatModel.find({ user: user.id })
+
+    res.status(200).json({
+        message: "Chats retrieved successfully",
+        chats
+    })
+}
+
+export async function getMessages(req, res) {
+    const { chatId } = req.params;
+
+    const chat = await chatModel.findOne({
+        _id: chatId,
+        user: req.user.id
+    })
+
+    if (!chat) {
+        return res.status(404).json({
+            message: "Chat not found"
+        })
+    }
+
+    const messages = await messageModel.find({
+        chat: chatId
+    })
+
+    res.status(200).json({
+        message: "Messages retrieved successfully",
+        messages
+    })
+}
+
+export async function deleteChat(req, res) {
+
+    const { chatId } = req.params;
+
+    const chat = await chatModel.findOneAndDelete({
+        _id: chatId,
+        user: req.user.id
+    })
+
+    await messageModel.deleteMany({
+        chat: chatId
+    })
+
+    if (!chat) {
+        return res.status(404).json({
+            message: "Chat not found"
+        })
+    }
+
+    res.status(200).json({
+        message: "Chat deleted successfully"
+    })
 }
--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,6 +1,6 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import {ChatMistralAI} from "@langchain/mistralai"
-import { HumanMessage } from "langchain"
+import { HumanMessage, SystemMessage, AIMessage} from "langchain"
 
 
 const geminiModel = new ChatGoogleGenerativeAI({
@@ -14,15 +14,32 @@ const mistralModel = new ChatMistralAI({
 })
 
 export async function generateResponse(message){
-    const response = await geminiModel.invoke([
-        new HumanMessage(message)
-    ])
-
-export async function generateMistralResponse(message){
-    const response = await mistralModel.invoke([
-        new SystemMessage(`You are a helpful assistant that provides concise and accurate answers to user queries. Always provide clear and informative responses, and avoid unnecessary details.`),
-    ])
+    const response = await geminiModel.invoke(message.map(msg => {
+                if (msg.role == "user") {
+                    return new HumanMessage(msg.content)
+                } else if (msg.role == "ai") {
+                    return new AIMessage(msg.content)
+                }
+            })) 
+        return response.text;
 }
+
+export async function generateChatTitle(message) {
+
+    const response = await mistralModel.invoke([
+        new SystemMessage(`
+            You are a helpful assistant that generates concise and descriptive titles for chat conversations.
+            
+            User will provide you with the first message of a chat conversation, and you will generate a title that captures the essence of the conversation in 2-4 words. The title should be clear, relevant, and engaging, giving users a quick understanding of the chat's topic.    
+        `),
+        new HumanMessage(`
+            Generate a title for a chat conversation based on the following first message:
+            "${message}"
+            `)
+    ])
 
     return response.text;
+
 }
+
+
